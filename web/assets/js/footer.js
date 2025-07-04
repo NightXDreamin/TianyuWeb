651fffdb1afab43be76d1f7c9092e536208424b6
AozoraANDER <609917460@qq.com>
2025-07-04 10:24:33 +0800

    联系我们页面更新

--- web/assets/js/footer.js
+++ web/assets/js/footer.js
@@ -1,38 +1,51 @@
 document.write(`
-  <footer id="site-footer">
-    <div class="container">
-      <div class="footer-grid">
-        <div class="footer-column">
-          <h4>天昱环保</h4>
-          <p>致力于成为您最信赖的环保工程合作伙伴。</p>
-        </div>
-        <div class="footer-column">
-          <h4>关于天昱</h4>
-          <ul>
-            <li><a href="#">核心团队</a></li>
-            <li><a href="#">公司资质</a></li>
-            <li><a href="#">加入我们</a></li>
-          </ul>
-        </div>
-        <div class="footer-column">
-          <h4>服务项目</h4>
-          <ul>
-            <li><a href="#">除尘脱硫</a></li>
-            <li><a href="#">水质检测</a></li>
-            <li><a href="#">环保培训</a></li>
-          </ul>
-        </div>
-        <div class="footer-column">
-          <h4>工程设备</h4>
-          <ul>
-            <li><a href="#">水处理设备</a></li>
-            <li><a href="#">气处理设备</a></li>
-          </ul>
-        </div>
-      </div>
-      <div class="footer-bottom">
-        <p>版权所有 &copy; 2025 河南天昱环保科技有限公司 | 豫ICP备XXXXXXXX号-1</p>
-      </div>
+<footer id="site-footer" class="footer">
+  <div class="footer__inner container">
+
+    <div class="footer__col footer__col--brand">
+      <img src="/web/assets/img/Logo带文字窄.png" alt="天昱环保 Logo" class="footer__logo" />
+      <h4 class="footer__company">河南天昱环保工程有限公司</h4>
+      <p class="footer__tagline">美好明天，你我共建！</p>
+
+      <ul class="footer__contact">
+        <li><span class="label">电话：</span><a href="tel:13007606387">13007606387</a></li>
+        <li><span class="label">邮箱：</span><a href="mailto:henantianyuhuanbao@163.com">henantianyuhuanbao@163.com</a></li>
+        <li><span class="label">地址：</span>河南省郑州市中原区桐柏路与陇海路交叉口凯旋门大厦1406</li>
+      </ul>
     </div>
-  </footer>
+
+    <div class="footer__col">
+      <h5 class="footer__title">关于天昱</h5>
+      <ul class="footer__nav">
+        <li><a href="/about.html#team">团队</a></li>
+        <li><a href="/cases.html">案例</a></li>
+        <li><a href="/about.html#career">职业</a></li>
+      </ul>
+    </div>
+
+    <div class="footer__col">
+      <h5 class="footer__title">服务类项目</h5>
+      <ul class="footer__nav">
+        <li><a href="/services.html#consult">咨询</a></li>
+        <li><a href="/services.html#test">检测</a></li>
+        <li><a href="/services.html#train">培训</a></li>
+      </ul>
+    </div>
+
+    <div class="footer__col">
+      <h5 class="footer__title">工程设备</h5>
+      <ul class="footer__nav">
+        <li><a href="/products.html#water">水处理</a></li>
+        <li><a href="/products.html#gas">气处理</a></li>
+        <li><a href="/products.html#noise">噪音控制</a></li>
+      </ul>
+    </div>
+
+  </div><div class="footer__bottom">
+    <p>
+      &copy;2025 河南天昱环保工程有限公司 |
+      <a href="https://beian.miit.gov.cn/" target="_blank" rel="noopener noreferrer">豫ICP备2023015121号-1</a>
+    </p>
+  </div>
+</footer>
 `);
