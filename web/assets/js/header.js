a613b229cbda3269919e298dc52776084364c1c6
AozoraANDER <609917460@qq.com>
2025-07-04 16:46:07 +0800

    关于天昱页面完成

--- web/assets/js/header.js
+++ web/assets/js/header.js
@@ -6,11 +6,11 @@ document.write(`
             </div>
             <nav class="main-nav" aria-label="主导航">
                 <ul>
-                    <li><a href="about.html">关于天昱</a></li>
-                    <li><a href="products.html">产品中心</a></li>
-                    <li><a href="cases.html">工程案例</a></li>
-                    <li><a href="news.html">新闻资讯</a></li>
-                    <li><a href="contact.html">联系我们</a></li>
+                    <li><a href="/about.html">关于天昱</a></li>
+                    <li><a href="/products.html">产品中心</a></li>
+                    <li><a href="/cases.html">工程案例</a></li>
+                    <li><a href="/news.html">新闻资讯</a></li>
+                    <li><a href="/contact.html">联系我们</a></li>
                 </ul>
             </nav>
             <div class="header-cta">
