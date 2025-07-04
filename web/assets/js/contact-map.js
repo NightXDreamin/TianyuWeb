55a8352ab184474fd6cd7bbc77a68f0ea908870f
AozoraANDER <609917460@qq.com>
2025-07-04 10:59:29 +0800

    联系我们页面修正

--- web/assets/js/contact-map.js
+++ web/assets/js/contact-map.js
@@ -1,67 +1,65 @@
-// assets/js/contact-map.js
+// assets/js/contact-map.js (修正版)
 
-function initMap() {
+function initContactMap() {
     const mapContainer = document.getElementById('map-container');
 
+    // 如果当前页面没有地图容器，就直接退出，防止在其他页面报错
     if (!mapContainer) {
-        // 如果页面上没有地图容器，就直接返回，防止报错
         return;
     }
 
     try {
-        var infoWindow, map, level = 16,
-            center = { lng: 113.612466, lat: 34.735581 },
-            features = [{
-                "icon": "cir",
-                "color": "red",
-                "name": "河南天昱环保工程有限公司",
-                "desc": "河南省郑州市中原区桐柏路与陇海路交叉口凯旋门大厦1406",
-                "lnglat": { "Q": 34.73558112897725, "R": 113.61246635645625, "lng": 113.612466, "lat": 34.735581 },
-                "offset": { "x": -9, "y": -31 },
-                "type": "Marker"
-            }];
+        const level = 16;
+        const center = { lng: 113.612466, lat: 34.735581 };
+        const features = [{
+            "icon": "cir",
+            "color": "red",
+            "name": "河南天昱环保工程有限公司",
+            "desc": "河南省郑州市中原区桐柏路与陇海路交叉口凯旋门大厦1406",
+            "lnglat": { "lng": 113.612466, "lat": 34.735581 },
+            "offset": { "x": -9, "y": -31 },
+            "type": "Marker"
+        }];
 
-        function loadFeatures() {
-            for (var feature, data, i = 0, len = features.length; i < len; i++) {
-                data = features[i];
-                if (data.type === "Marker") {
-                    feature = new AMap.Marker({
-                        map: map,
-                        position: new AMap.LngLat(data.lnglat.lng, data.lnglat.lat),
-                        zIndex: 3,
-                        extData: data,
-                        offset: new AMap.Pixel(data.offset.x, data.offset.y),
-                        title: data.name,
-                        content: '<div class="icon icon-' + data.icon + ' icon-' + data.icon + '-' + data.color + '"></div>'
-                    });
-                    if (feature) {
-                        AMap.event.addListener(feature, "click", mapFeatureClick);
-                    }
-                }
-            }
-        }
-
-        function mapFeatureClick(e) {
-            if (!infoWindow) {
-                infoWindow = new AMap.InfoWindow({ autoMove: true, isCustom: false });
-            }
-            var extData = e.target.getExtData();
-            infoWindow.setContent("<div class='myinfowindow'><h5>" + extData.name + "</h5><div>" + extData.desc + "</div></div>");
-            infoWindow.open(map, e.lnglat);
-        }
-
-        // 关键改动：确保地图在正确的容器ID "map-container" 中初始化
-        map = new AMap.Map("map-container", {
+        // 初始化地图实例
+        const map = new AMap.Map("map-container", {
             center: new AMap.LngLat(center.lng, center.lat),
-            zoom: level, // 使用了变量 level，而不是固定的数字
+            zoom: level,
             keyboardEnable: true,
             dragEnable: true,
             scrollWheel: true,
             doubleClickZoom: true
         });
 
-        loadFeatures();
+        let infoWindow = null;
 
+        // 点击标记点时触发的事件
+        function mapFeatureClick(e) {
+            if (!infoWindow) {
+                infoWindow = new AMap.InfoWindow({ autoMove: true, isCustom: false });
+            }
+            const extData = e.target.getExtData();
+            infoWindow.setContent(`<div class='myinfowindow'><h5>${extData.name}</h5><div>${extData.desc}</div></div>`);
+            infoWindow.open(map, e.lnglat);
+        }
+
+        // 加载所有标记点
+        features.forEach(data => {
+            if (data.type === "Marker") {
+                const marker = new AMap.Marker({
+                    map: map,
+                    position: new AMap.LngLat(data.lnglat.lng, data.lnglat.lat),
+                    zIndex: 3,
+                    extData: data,
+                    offset: new AMap.Pixel(data.offset.x, data.offset.y),
+                    title: data.name,
+                    content: `<div class="icon icon-${data.icon} icon-${data.icon}-${data.color}"></div>`
+                });
+                marker.on("click", mapFeatureClick);
+            }
+        });
+
+        // 地图加载完成后，添加工具条等插件
         map.on('complete', function () {
             map.plugin(["AMap.ToolBar", "AMap.OverView", "AMap.Scale"], function () {
                 map.addControl(new AMap.ToolBar({ ruler: true, direction: true, locate: false }));
@@ -76,5 +74,7 @@ function initMap() {
     }
 }
 
-// 确保在DOM加载完成后再执行初始化
-document.addEventListener('DOMContentLoaded', initMap);
+// 确保在DOM加载完成后再执行我们的地图初始化函数
+// 我们不再需要 document.addEventListener 了，因为 main.js 已经有了
+// 直接调用函数即可，但要确保 main.js 是最后加载的
+initContactMap();
